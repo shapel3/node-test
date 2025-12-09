d41406401d6514417f52eb68ece8aa602ea5062d
Shapel Nahyrych <shapel.nahyrych@gmail.com>
2025-12-09 21:03:19 +0000

    practice props

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 import cowsay from "cowsay";
 import Article from "./Article.mjs";
-import {mult, del} from "./maths.mjs"
+import {mult, sum} from "./maths.mjs"
 
 console.log(cowsay.say({text: 'qwerty'}));
 
@@ -13,7 +13,7 @@ const num2 = 8;
 console.log(sum(num1, num2));
 console.log(mult(4,9));
 
-const newArticle = new Article('Content for article');
+const newArticle = new Article('Content for article!');
 console.log(newArticle.render());
 
 
